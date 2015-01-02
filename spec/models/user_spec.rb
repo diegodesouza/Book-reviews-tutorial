6cb7f8d7a64916eebe4f0e5251f6a996d6e0ad6c
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-01 21:01:20 -0500

    refactor test based on :review association in factorygirl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,16 +6,9 @@ describe User do
     it 'returns the most recent reviews ordered by creation time' do
       user = FactoryGirl.create(:user)
 
-      first_book = FactoryGirl.create(:book)
-      second_book = FactoryGirl.create(:book)
-      third_book = FactoryGirl.create(:book)
-
-      oldest_review = FactoryGirl.create(:review, user: user,
-      book: first_book, created_at: '2012-04-04')
-      newest_review = FactoryGirl.create(:review, user: user,
-      book: second_book, created_at: '2013-07-01')
-      middle_review = FactoryGirl.create(:review, user: user,
-      book: third_book, created_at: '2013-02-12')
+      oldest_review = FactoryGirl.create(:review, created_at: '2012-04-04')
+      newest_review = FactoryGirl.create(:review, created_at: '2013-07-01')
+      middle_review = FactoryGirl.create(:review, created_at: '2013-02-12')
 
       expect(user.recent_reviews).to eq([newest_review, middle_review, oldest_review])
     end
