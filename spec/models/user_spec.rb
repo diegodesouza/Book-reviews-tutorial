880cfaab5f145a55f64b03e606dd73dfe2d6c44f
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-01 21:07:41 -0500

    add user to test, based on association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,9 @@ describe User do
     it 'returns the most recent reviews ordered by creation time' do
       user = FactoryGirl.create(:user)
 
-      oldest_review = FactoryGirl.create(:review, created_at: '2012-04-04')
-      newest_review = FactoryGirl.create(:review, created_at: '2013-07-01')
-      middle_review = FactoryGirl.create(:review, created_at: '2013-02-12')
+      oldest_review = FactoryGirl.create(:review, user: user, created_at: '2012-04-04')
+      newest_review = FactoryGirl.create(:review, user: user, created_at: '2013-07-01')
+      middle_review = FactoryGirl.create(:review, user: user, created_at: '2013-02-12')
 
       expect(user.recent_reviews).to eq([newest_review, middle_review, oldest_review])
     end
